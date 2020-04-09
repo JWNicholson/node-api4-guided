a56dbca0d7fb9826b9621960a0089d1d6c16fa1e
JW Nicholson <john.nicholson812@gmail.com>
2020-04-09 14:00:11 -0400

    e

--- api/api-router.js
+++ api/api-router.js
@@ -9,7 +9,7 @@ router.use(express.json());
 router.get("/", (req, res) => {
  
   const message = process.env.MESSAGE || "Hello from localhost";
-  res.status(200).json({ api: "up", message });
+  res.status(200).json({ api:  message });
 
 });
 
